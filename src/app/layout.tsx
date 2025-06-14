f3cebd6f047eedc0b6e62a0b520e1d0e9b3043ce
syedfardeenjeelani <syedfardeenjeelani13@gmail.com>
2025-06-14 14:54:16 +0530

    fix: fixed build err and removed footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Sora } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
